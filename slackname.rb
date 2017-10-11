0a0e81ca931da02032d43a228cef1aaac5ef5956
Adam Ochonicki <fromonesrc@users.noreply.github.com>
2017-10-10 20:50:32 -0400

    Multi-teams and drop more emoji

--- slackname.rb
+++ slackname.rb
@@ -6,8 +6,8 @@ require 'optparse'
 
 Dotenv.load
 
-token = ENV['SLACK_TOKEN']
-user  = ENV['SLACK_USER']
+tokens = ENV['SLACK_TOKENS'].split(',')
+users  = ENV['SLACK_USERS'].split(',')
 
 optparse = OptionParser.new
 optparse.parse!
@@ -15,7 +15,7 @@ status_text = ARGV.join(" ")
 
 emoji = %w{
   +1 100 8ball admission_tickets aerial_tramway airplane alarm_clock
-  alembic alien ambulance amphora anchor angel ant apple
+  alembic alien amphora anchor angel ant apple
   arrow_right_hook art articulated_lorry athletic_shoe atom_symbol
   baby_bottle baby_chick badminton_racquet_and_shuttlecock balloon
   ballot_box_with_ballot ballot_box_with_check bamboo banana barber
@@ -33,7 +33,7 @@ emoji = %w{
   children_crossing chipmunk chocolate_bar christmas_tree church
   cinema circus_tent city_sunrise city_sunset cityscape clap clapper
   classical_building clipboard closed_book closed_lock_with_key
-  closed_umbrella cloud cocktail coffee coffin comet compression
+  closed_umbrella cloud cocktail coffee comet compression
   computer confetti_ball construction control_knobs cookie corn cow2
   cow crab credit_card crescent_moon cricket_bat_and_ball crocodile
   crossed_flags crystal_ball curly_loop curry custard
@@ -47,16 +47,16 @@ emoji = %w{
   evergreen_tree expressionless eye eyeglasses
   eyes face_with_rolling_eyes facepunch fallen_leaf fax feet
   ferris_wheel ferry field_hockey_stick_and_ball file_cabinet
-  film_frames film_projector fire fire_engine fireworks
+  film_frames film_projector fire fireworks
   first_quarter_moon first_quarter_moon_with_face fish fish_cake
   fishing_pole_and_fish fist flags flashlight fleur_de_lis floppy_disk
-  flower_playing_cards fog foggy football footprints 
+  flower_playing_cards fog foggy football footprints
   fountain four_leaf_clover frame_with_picture fried_shrimp fries frog
   fuelpump full_moon full_moon_with_face funeral_urn game_die gear
   gem ghost gift goat golf golfer grapes green_apple green_book
   green_heart grin grinning guardsman guitar haircut hamburger
   hammer hammer_and_pick hammer_and_wrench hamster hand handbag hash
-  hatched_chick hatching_chick headphones hear_no_evil heart_eyes
+  hatched_chick hatching_chick headphones heart_eyes
   heart_eyes_cat helicopter helmet_with_white_cross herb hibiscus
   high_heel honey_pot horse horse_racing hot_pepper hotdog
   hotsprings hourglass hourglass_flowing_sand house house_buildings
@@ -81,14 +81,14 @@ emoji = %w{
   new_moon_with_face newspaper night_with_stars no_entry no_entry_sign
   no_mouth nose notebook notebook_with_decorative_cover notes
   nut_and_bolt ocean octopus oden office ok_hand ok_woman old_key
-  om_symbol oncoming_automobile oncoming_bus oncoming_police_car
+  om_symbol oncoming_automobile oncoming_bus
   oncoming_taxi open_hands orange_book ox package page_facing_up
-  page_with_curl pager palm_tree panda_face paperclip partly_sunny
+  page_with_curl pager panda_face paperclip partly_sunny
   partly_sunny_rain passenger_ship peach pear pencil2 penguin
   performing_arts person_with_ball
   person_with_pouting_face phone pick pig2 pig pig_nose pill
   pineapple pizza point_down point_left point_right point_up
-  point_up_2 police_car poodle popcorn post_office postal_horn pouch
+  point_up_2 poodle popcorn post_office postal_horn pouch
   poultry_leg pound pray prayer_beads princess printer purple_heart
   purse pushpin rabbit2 rabbit racehorse racing_car
   racing_motorcycle radio radioactive_sign railway_car rain_cloud
@@ -98,25 +98,24 @@ emoji = %w{
   roller_coaster rooster rose rosette rotating_light round_pushpin
   rowboat rugby_football runner running_shirt_with_sash sake sandal
   santa satellite satellite_antenna saxophone scales school_satchel
-  scissors scorpion see_no_evil seedling shamrock
+  scissors scorpion seedling shamrock
   shaved_ice sheep shell shinto_shrine ship shirt shower ski
   skier sleuth_or_spy slightly_smiling_face slot_machine
   small_airplane smile smile_cat smiley smiley_cat smiling_imp
   smirk smirk_cat smoking snail snake snow_capped_mountain
   snow_cloud snowboarder snowflake snowman snowman_without_snow
   soccer space_invader spaghetti sparkle sparkler sparkles
-  sparkling_heart speak_no_evil speech_balloon speedboat spider
+  sparkling_heart speech_balloon speedboat spider
   spider_web spiral_calendar_pad spiral_note_pad spock-hand
   sports_medal stadium stars station statue_of_liberty
-  steam_locomotive stew stopwatch strawberry stuck_out_tongue
-  stuck_out_tongue_closed_eyes stuck_out_tongue_winking_eye
+  steam_locomotive stew stopwatch strawberry
   studio_microphone sun_with_face sunflower sunglasses sunny
   sunrise sunrise_over_mountains surfer sushi suspension_railway
   sweat_drops sweat_smile sweet_potato swimmer syringe
   table_tennis_paddle_and_ball taco tada tanabata_tree tangerine
   taxi tea telephone_receiver telescope tennis tent the_horns
   thermometer thinking_face thought_balloon thunder_cloud_and_rain
-  ticket tiger2 tiger timer_clock tokyo_tower tomato tongue
+  ticket tiger2 tiger timer_clock tokyo_tower tomato
   tophat tornado tractor traffic_light train2 train tram
   triangular_flag_on_post triangular_ruler trolleybus trophy
   tropical_drink tropical_fish truck trumpet tulip turkey turtle
@@ -134,18 +133,20 @@ emoji = %w{
 
 emoji = emoji.sample
 
-#status_text = emoji if status_text == ""
-
 status_emoji = emoji.prepend("%3A")
 status_emoji << "%3A"
 
-presence_response = `curl -s 'https://slack.com/api/users.getPresence?token=#{token}&user=#{user}'`
-presence = JSON.parse(presence_response)['presence']
+token_users = tokens.zip(users)
 
+token_users.each do |token, user|
+  presence_response = `curl -s 'https://slack.com/api/users.getPresence?token=#{token}&user=#{user}'`
+  presence = JSON.parse(presence_response)['presence']
 
-profile = `curl -s 'https://slack.com/api/users.profile.get?token=#{token}'`
-status_text = JSON.parse(profile)['profile']['status_text']
+  profile = `curl -s 'https://slack.com/api/users.profile.get?token=#{token}'`
 
-if presence == 'active' && status_text.empty?
-  curl = `curl -s -XPOST 'https://slack.com/api/users.profile.set?token=#{token}&profile=%7B%22status_emoji%22%3A%22#{status_emoji}%22%2C%22status_text%22%3A%22#{status_text}%22%7D'`
+  status_text = JSON.parse(profile)['profile']['status_text']
+
+  if presence == 'active' && status_text.empty?
+    curl = `curl -s -XPOST 'https://slack.com/api/users.profile.set?token=#{token}&profile=%7B%22status_emoji%22%3A%22#{status_emoji}%22%2C%22status_text%22%3A%22#{status_text}%22%7D'`
+  end
 end
